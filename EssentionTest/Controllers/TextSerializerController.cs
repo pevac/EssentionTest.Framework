414cbcc0d4c9338aa9fad4b169b78f1a89dc4fb7
pevac <home.peva@gmail.com>
2017-10-02 12:05:00 +0300

    delete create file

--- EssentionTest/Controllers/TextSerializerController.cs
+++ EssentionTest/Controllers/TextSerializerController.cs
@@ -17,26 +17,11 @@ namespace EssentionTest.Controllers
             {
                 var formatText = TextSerializer.SerializeToCvs(value.Text, value.SeparatorCvs);
 
-                var folder = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/Files");
-                Directory.CreateDirectory(folder);
-
-                using (var fileStream = new FileStream($"{folder}/cvsFile{(DateTime.Now - new DateTime()).Ticks}.cvs", FileMode.OpenOrCreate, FileAccess.ReadWrite))
-                {
-                    using (var streamWriter = new StreamWriter(fileStream))
-                    {
-                        streamWriter.Write(formatText);
-                    }
-                }
-
                 return Ok(new { formatText });
             }
             else if (value.FormatType == "xml")
             {
                 var xmlText = TextSerializer.SerializeToXml(value.Text);
-
-                var folder = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/Files");
-                Directory.CreateDirectory(folder);
-                xmlText.Save($"{folder}/xmlFile{(DateTime.Now - new DateTime()).Ticks}.xml");
 
                 var formatText = xmlText.Root.ToString();
 
